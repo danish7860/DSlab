508431346886be1b77bbf15ded1a504c35f9c9fd
danish7860 <danish7860danny@gmail.com>
2017-08-21 11:46:55 +0530

    Update T.c

--- T.c
+++ T.c
@@ -20,22 +20,22 @@ void main()
 char str[MAX],resstr[MAX],temp[MAX];
 int i,z;
 clrscr();
-printf(“nEnter a prefix string?”);
+printf(“\nEnter a prefix string”);
 gets(str);
 for(i=0;str[i]!=’\0';i++);
 for(i=i-1,z=0;i>=0;z++,i–)
 temp[z]=str[i];
 temp[z]=’\0';
-printf(“nPrefix string: %s”,str);
+printf(“\nPrefix string: %s”,str);
 prefix_to_postfix(temp,resstr);
-printf(“nPostfix string: %s”,resstr);
+printf(“\nPostfix string: %s”,resstr);
 getch();
 }
 
 void push(struct stack *s,char ch)
 {
 if(s->top==MAX-1)
-{printf(“Stack Overflow!”);
+{printf(“Stack Overflow”);
 exit(1);
 }
 s->arr[++(s->top)]=ch;
@@ -44,14 +44,14 @@ s->arr[++(s->top)]=ch;
 char pop(struct stack *s)
 {
 if(s->top==-1)
-{printf(“Stack Underflow!”);
+{printf(“Stack Underflow”);
 exit(1);
 }
 return(s->arr[(s->top)–]);
 }
 
 int isdigit(char ch)
-{return(ch>=’0′ && ch<=’9′);
+{return(ch>='0' && ch<='9');
 }
 
 void prefix_to_postfix(char instr[],char outstr[])
@@ -60,7 +60,7 @@ int i,j,ct=1,z=0;
 char ch,opnd1,opnd2;
 struct stack stk;
 stk.top=-1;
-for(i=0;instr[i]!=’\0';i++)
+for(i=0;instr[i]!='\0';i++)
 {
 ch=instr[i];
 if(isdigit(ch))
@@ -84,5 +84,5 @@ outstr[z++]=ch;
 }
 }
 }
-outstr[z]=’\0';
+outstr[z]='\0';
 }
